ddcbb47291941d34239747bbcb8e5e736fa6ee3a
Seogun95 <ksj083895@gmail.com>
2023-02-07 05:30:10 +0900

    feat: todo 카드 구조 변경

--- src/component/TodoCard.tsx
+++ src/component/TodoCard.tsx
@@ -1,21 +1,31 @@
 import React, { useState } from 'react';
-import styles from 'App.module.css';
+import styles from '../App.module.css';
+import { FiTrash2 } from 'react-icons/fi';
 
 interface TodoProps {
   title: string;
   body: string;
   btnfn: (e: React.MouseEvent<HTMLElement>) => void;
   deleteBtn: (e: React.MouseEvent<HTMLElement>) => void;
-  btnText: string;
+  btnText: JSX.Element;
+  bgcolor: string;
 }
 
-function TodoCard({ title, body, btnfn, btnText, deleteBtn }: TodoProps) {
+function TodoCard({ title, body, btnfn, btnText, deleteBtn, bgcolor }: TodoProps) {
   return (
-    <div>
-      <h3>{title}</h3>
-      <h4>{body}</h4>
-      <button onClick={btnfn}>{btnText}</button>
-      <button onClick={deleteBtn}>삭제</button>
+    <div className={styles.todo__cardBox}>
+      <div style={{ padding: '2rem' }}>
+        <p className={styles.todo__title}>{title}</p>
+        <p className={styles.todo__desc}>{body}</p>
+      </div>
+      <div className={styles.todo__cardBox__button_box}>
+        <button style={{ backgroundColor: bgcolor }} onClick={btnfn}>
+          {btnText}
+        </button>
+        <button style={{ backgroundColor: '#fe645c' }} onClick={deleteBtn}>
+          <FiTrash2 />
+        </button>
+      </div>
     </div>
   );
 }
