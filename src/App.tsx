e6e3d0fb408c351e53cc0b681a5dfe4b9e971cdb
Seogun95 <ksj083895@gmail.com>
2023-02-07 13:12:51 +0900

    fix: App.tsx 리팩터링 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import React, { useState } from 'react';
-import { HiPlus } from 'react-icons/hi';
-import { FaPencilAlt, FaCheck } from 'react-icons/fa';
+import { FaPencilAlt } from 'react-icons/fa';
 import { FcApproval, FcClock } from 'react-icons/fc';
-import { RiArrowGoBackLine } from 'react-icons/ri';
-import { FiTrash2 } from 'react-icons/fi';
-import styles from './App.module.css';
-import './reset.css';
+import styles from './style/App.module.css';
+import Input from './components/Input';
+import TodoCard from './components/TodoCard';
+import TodoDoneCard from './components/TodoDoneCard';
+import './style/reset.css';
 
 function App() {
-  //useState는 값을 정하기 위해 만드는 것이다.
-  const [todoInput, setTodoInput] = useState('');
-  const [textInput, setTextInput] = useState('');
-  //todo title
   const [todoTitle, setTodoTitle] = useState([
     { id: 0, text: 'React 완강 목표', body: '노마드코더 마스터클래스 2월 30일까지', isdone: false },
   ]);
-  //done state
+
   const [todoDone, setTodoDone] = useState([
     {
       id: 1,
@@ -25,104 +21,13 @@ function App() {
     },
   ]);
 
-  const inputTodo = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { value },
-    } = event; // event.target.value 와 같은 새로운 신문법이다.
-    setTodoInput(value);
-  };
-
-  const textTodo = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { value },
-    } = event;
-    setTextInput(value);
-  };
-
-  //TODO: TS에서 제출 버튼 눌렀을때, 빈값 출력 버그 수정!!!
-  const submitBtnHandler = () => {
-    if (todoInput === '') {
-      alert('할 일은 필수 사항 입니다.');
-      return;
-    } else {
-      const obj = {
-        id: Date.now(),
-        text: todoInput,
-        body: textInput,
-        isdone: false,
-      };
-      // 배열안에서 스프레드로 이전에 있던 값 todoTitle에 새로운 값 obj를 추가한다.
-      // 그리고 state값이 변경 되면서 새로운 주소값을 할당받아 리렌더링이 일어난다. 이것이 불변셩
-      setTodoTitle([...todoTitle, obj]);
-      setTodoInput('');
-      setTextInput('');
-    }
-  };
-
-  // 완료 버튼: 할 일 ➜ 완료한 일로 이동
-  const moveToDone = (id: number) => {
-    let deleteTodo = todoTitle.filter((_, i) => todoTitle[i].id !== id);
-    let addToDone = todoTitle.filter((_, i) => todoTitle[i].id === id); //할 일 ➜ 완료한 일로 이동
-    addToDone[0].isdone = true;
-    setTodoTitle(deleteTodo);
-    setTodoDone([...todoDone, ...addToDone]);
-  };
-
-  // 취소 버튼: 완료한일 ➜ 할 일로 이동
-  const moveToTodo = (id: number) => {
-    let deleteDone = todoDone.filter((_, i) => todoDone[i].id !== id);
-    let addTodoDone = todoDone.filter((_, i) => todoDone[i].id === id);
-    addTodoDone[0].isdone = false;
-    setTodoDone(deleteDone);
-    setTodoTitle([...todoTitle, ...addTodoDone]);
-  };
-
-  // 삭제 버튼: 할 일 삭제 버튼
-  const deleteTodo = (id: number) => {
-    // filter는 새배열을 만들어 배열을 반환 했기 때문에 구조분해 할당 할 필요 없다. (이전에 있던 배열을 버림)
-    let deleteList = todoTitle.filter((_, i) => todoTitle[i].id !== id);
-    setTodoTitle(deleteList);
-  };
-
-  // 삭제 버튼: 완료한 일 삭제 버튼
-  const deleteDoneside = (id: number) => {
-    let deleteDoneList = todoDone.filter((_, i) => todoDone[i].id !== id);
-    setTodoDone(deleteDoneList);
-  };
-
   return (
     <div className={styles.todo__layout}>
       <div className={styles.todo__container}>
         <h1>
           TODO LIST <FaPencilAlt />
         </h1>
-        <div className={styles.todo__input__container}>
-          <label htmlFor={'todoInput'}>
-            <span>* </span>할 일
-          </label>
-          <input
-            id={'todoInput'}
-            className={styles.todo__input}
-            value={todoInput}
-            onChange={inputTodo}
-            placeholder={'할 일을 작성해주세요'}
-          />
-        </div>
-        <div className={styles.todo__input__container}>
-          <label htmlFor={'textInput'}>
-            <span>* </span>자세히
-          </label>
-          <input
-            id={'textInput'}
-            className={styles.todo__input}
-            value={textInput}
-            onChange={textTodo}
-            placeholder={'할 일을 보충할 말을 작성해주세요'}
-          />
-        </div>
-        <button className={styles.todo__submit} onClick={submitBtnHandler}>
-          <HiPlus />
-        </button>
+        <Input setTodoTitle={setTodoTitle} todoTitle={todoTitle} />
       </div>
       <div className={styles.todo__box__continaer}>
         <div className={styles.todo__box}>
@@ -130,32 +35,19 @@ function App() {
             <FcClock />
             안했다!
           </h4>
-          {/*map 함수를 사용하여 바인딩을 한다*/}
-          {/*map사용할때는 반드시 Key값이 들어가야한다.*/}
           {todoTitle.map((a, i) => {
-            /*key는 고유 식별자 이기 때문에 prop으로 넘길 필요 없다.*/
             return (
-              <div
-                className={styles.todo__cardBox}
-                style={{ border: a.isdone ? '3px solid #9ed260' : '3px solid #fe645c' }}
+              <TodoCard
                 key={i}
-              >
-                <div style={{ padding: '2rem' }}>
-                  <p className={styles.todo__title}>{a.text}</p>
-                  <p className={styles.todo__desc}>{a.body}</p>
-                </div>
-                <div className={styles.todo__cardBox__button_box}>
-                  <button
-                    style={{ backgroundColor: a.isdone ? '#fe645c' : '#ffc332' }}
-                    onClick={() => moveToDone(a.id)}
-                  >
-                    {a.isdone ? <RiArrowGoBackLine /> : <FaCheck />}
-                  </button>
-                  <button style={{ backgroundColor: '#ff493f' }} onClick={() => deleteTodo(a.id)}>
-                    <FiTrash2 />
-                  </button>
-                </div>
-              </div>
+                todoTitle={todoTitle}
+                setTodoTitle={setTodoTitle}
+                setTodoDone={setTodoDone}
+                todoDone={todoDone}
+                text={a.text}
+                body={a.body}
+                id={a.id}
+                isdone={a.isdone}
+              />
             );
           })}
         </div>
@@ -166,30 +58,17 @@ function App() {
           </h4>
           {todoDone.map((a, i) => {
             return (
-              <div
+              <TodoDoneCard
                 key={i}
-                className={styles.todo__cardBox}
-                style={{ border: a.isdone ? '3px solid #9ed260' : '3px solid #fe645c' }}
-              >
-                <div style={{ padding: '2rem' }}>
-                  <p className={styles.todo__title}>{a.text}</p>
-                  <p className={styles.todo__desc}>{a.body}</p>
-                </div>
-                <div className={styles.todo__cardBox__button_box}>
-                  <button
-                    style={{ backgroundColor: a.isdone ? '#fe645c' : '#ffc332' }}
-                    onClick={() => moveToTodo(a.id)}
-                  >
-                    {a.isdone ? <RiArrowGoBackLine /> : <FaCheck />}
-                  </button>
-                  <button
-                    style={{ backgroundColor: '#ff493f' }}
-                    onClick={() => deleteDoneside(a.id)}
-                  >
-                    <FiTrash2 />
-                  </button>
-                </div>
-              </div>
+                todoTitle={todoTitle}
+                setTodoTitle={setTodoTitle}
+                setTodoDone={setTodoDone}
+                todoDone={todoDone}
+                text={a.text}
+                body={a.body}
+                id={a.id}
+                isdone={a.isdone}
+              />
             );
           })}
         </div>
