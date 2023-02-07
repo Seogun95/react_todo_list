faa4d95777646d6e3a4aef86e8d5a29557286622
Seogun95 <ksj083895@gmail.com>
2023-02-07 13:13:39 +0900

    feat: TodoCard.tsx 리팩터링 완료

---
+++ src/components/TodoCard.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import styles from '../style/TodoCard.module.css';
+import { FiTrash2 } from 'react-icons/fi';
+import { RiArrowGoBackLine } from 'react-icons/ri';
+import { FaCheck } from 'react-icons/fa';
+
+interface Userprops {
+  id: number;
+  text: string;
+  body: string;
+  isdone: boolean;
+}
+
+interface TodoProps {
+  todoTitle: Userprops[];
+  setTodoTitle: (e: Userprops[]) => void;
+  setTodoDone: (e: Userprops[]) => void;
+  todoDone: Userprops[];
+  text: string;
+  body: string;
+  id: number;
+  isdone: boolean;
+}
+
+const TodoCard = ({
+  text,
+  body,
+  id,
+  todoTitle,
+  setTodoTitle,
+  setTodoDone,
+  todoDone,
+  isdone,
+}: TodoProps) => {
+  const moveToDone = (id: number) => {
+    let deleteTodo = todoTitle.filter((_, i) => todoTitle[i].id !== id);
+    let addToDone = todoTitle.filter((_, i) => todoTitle[i].id === id);
+    addToDone[0].isdone = true;
+    setTodoTitle(deleteTodo);
+    setTodoDone([...todoDone, ...addToDone]);
+  };
+
+  const deleteTodo = (id: number) => {
+    let deleteList = todoTitle.filter((_, i) => todoTitle[i].id !== id);
+    setTodoTitle(deleteList);
+  };
+
+  return (
+    <div
+      className={styles.todo__cardBox}
+      style={{ border: isdone ? '3px solid #9ed260' : '3px solid #fe645c' }}
+    >
+      <div style={{ padding: '2rem' }}>
+        <p className={styles.todo__title}>{text}</p>
+        <p className={styles.todo__desc}>{body}</p>
+      </div>
+      <div className={styles.todo__cardBox__button_box}>
+        <button
+          style={{ backgroundColor: isdone ? '#fe645c' : '#ffc332' }}
+          onClick={() => moveToDone(id)}
+        >
+          {isdone ? <RiArrowGoBackLine /> : <FaCheck />}
+        </button>
+        <button style={{ backgroundColor: '#ff493f' }} onClick={() => deleteTodo(id)}>
+          <FiTrash2 />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoCard;
